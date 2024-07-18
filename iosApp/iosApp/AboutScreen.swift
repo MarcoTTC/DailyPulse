24e272489b953686d71da1acc10d1b69cb031e95
Marco Coelho <marco.tulio.todeschini.coelho@gmail.com>
2024-07-18 10:47:22 -0300

    Adding the first screen, about device screen

---
+++ iosApp/iosApp/AboutScreen.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+struct AboutScreen: View {
+    var body: some View {
+        NavigationStack {
+            AboutListView()
+                .navigationTitle("About Device")
+        }
+    }
+}
--- shared/src/commonMain/kotlin/br/com/marcottc/dailypulse/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package br.com.marcottc.dailypulse
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
