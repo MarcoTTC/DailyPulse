6128153b90debf4f4ff2954ae033e5ea3651abe9
Marco Coelho <marco.tulio.todeschini.coelho@gmail.com>
2024-07-24 19:11:20 -0300

    Adding a toolbar to the iOS app

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -3,9 +3,25 @@ import shared
 
 struct ContentView: View {
 
+    @State private var shouldOpenAbout = false
+
 	var body: some View {
-		ArticleScreen(viewModel: .init())
-	}
+	    NavigationStack {
+		    ArticleScreen(viewModel: .init())
+	            .toolbar {
+	                ToolbarItem {
+	                    Button {
+	                        shouldOpenAbout = true
+	                    } label: {
+	                        Label("About", systemIamge: "info.circle").labelStyle()
+	                    }
+	                    .popover(isPresented: $shouldOpenAbout) {
+	                        AboutScreen()
+	                    }
+	                }
+	            }
+	    }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
