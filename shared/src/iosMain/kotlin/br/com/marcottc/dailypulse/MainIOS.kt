a7e443d322b3a108934cd8a22812a302c58a3074
Marco Coelho <marco.tulio.todeschini.coelho@gmail.com>
2024-08-01 18:23:37 -0300

    Porting the AboutScreen into compose multiplatform

--- shared/src/iosMain/kotlin/br/com/marcottc/dailypulse/MainIOS.kt
+++ shared/src/iosMain/kotlin/br/com/marcottc/dailypulse/MainIOS.kt
@@ -2,7 +2,8 @@ package br.com.marcottc.dailypulse
 
 import br.com.marcottc.dailypulse.ui.App
 import androidx.compose.ui.window.ComposeUIViewController
+import br.com.marcottc.dailypulse.ui.screens.AboutScreen
 
 fun MainViewController() = ComposeUIViewController {
-    App()
+    AboutScreen()
 }
